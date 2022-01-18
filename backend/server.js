dd94d8e33eee0ea4cd67204a5d4e587cc97b0a99
m4rtin7 <m.zavadzan@gmail.com>
2022-01-18 21:46:44 +0100

    heroku setup

--- backend/server.js
+++ backend/server.js
@@ -1,21 +1,21 @@
-const express = require('express');
-const bodyParser = require('body-parser');
-const database = require("./database/queries.js");
-const config = require("./config.json");
-const auth = require("./middleware/auth");
-const cron = require('node-cron');
+const express = require('express')
+const bodyParser = require('body-parser')
+const database = require('./database/queries.js')
+const config = require('./config.json')
+const auth = require('./middleware/auth')
+const cron = require('node-cron')
 
-const app = express();
+const app = express()
 
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: false }))
 
-app.get('/oblasti',auth ,database.oblasti.getAll)
-app.post('/pridajoblasti',auth ,database.oblasti.add)
+app.get('/oblasti', auth, database.oblasti.getAll)
+app.post('/pridajoblasti', auth, database.oblasti.add)
 
 app.get('/zamestnanci', auth, database.zamestnanci.getAll)
 app.get('/zamestnanecbyid', auth, database.zamestnanci.getByID)
-app.get('/pozicie',auth, database.zamestnanci.getAllpoz)
+app.get('/pozicie', auth, database.zamestnanci.getAllpoz)
 app.put('/upravzamestnanca', auth, database.zamestnanci.updateUser)
 app.delete('/zmazzamestnanca', auth, database.zamestnanci.deleteUser)
 app.post('/pridajzamestnanca', auth, database.zamestnanci.addUser)
@@ -26,34 +26,43 @@ app.post('/pridajskolenie', auth, database.skolenia.addSkolenie)
 app.put('/upravskolenie', auth, database.skolenia.updateSkolenie)
 app.get('/vsetkyskolenia', auth, database.skolenia.getAllSkolenia)
 app.get('/konciaceskolenia', auth, database.skolenia.endingSkolenia)
-app.post('/pridajskoleniezamestnancom', auth, database.skolenia.addSkolenieZamest)
-app.post('/upravskoleniezamestnancovi', auth, database.skolenia.updateSkolenieZam)
+app.post(
+  '/pridajskoleniezamestnancom',
+  auth,
+  database.skolenia.addSkolenieZamest
+)
+app.post(
+  '/upravskoleniezamestnancovi',
+  auth,
+  database.skolenia.updateSkolenieZam
+)
 
 app.put('/addoblasti', auth, database.user.addOblast)
-app.post("/register", auth, database.user.register)
+app.post('/register', auth, database.user.register)
 app.put('/upravheslo', auth, database.user.updatePasswd)
-app.post("/login", database.user.login)
+app.post('/login', database.user.login)
 app.post('/resetheslo', database.user.resetPasswd)
 
+app.get('/welcome', auth, (req, res) => {
+  console.log(req.user)
+  res.status(200).send('Welcome <3')
+})
 
-app.get("/welcome", auth, (req, res) => {
-    console.log(req.user);
-    res.status(200).send("Welcome <3");
-});
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static('../frontend/build '))
+}
 
-
-
-app.listen(config["API_PORT"], function () {
-    // let host = server.address().address
-    // let port = server.address().port
-    console.log ('Listen on port 9000');
-    // cron.schedule('* * * * *', function() {
-    //     database.user.notify().then();
-    //     console.log('running a task every minute');
-    // });
-    //3 den v mesiaci
-    // cron.schedule('0 0 3 * *', function() {
-    //     database.user.notify().then();
-    //     console.log('running a task every minute');
-    // });
+app.listen(process.env.PORT || config['API_PORT'], function () {
+  // let host = server.address().address
+  // let port = server.address().port
+  console.log('Listen on port 9000')
+  // cron.schedule('* * * * *', function() {
+  //     database.user.notify().then();
+  //     console.log('running a task every minute');
+  // });
+  //3 den v mesiaci
+  // cron.schedule('0 0 3 * *', function() {
+  //     database.user.notify().then();
+  //     console.log('running a task every minute');
+  // });
 })
