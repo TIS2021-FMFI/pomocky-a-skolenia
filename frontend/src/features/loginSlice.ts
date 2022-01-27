67835740d28a016f20b0a6253a1895c8d9589aee
msabolova149 <61238834+msabolova149@users.noreply.github.com>
2022-01-27 21:53:41 +0100

    loginslice

--- frontend/src/features/loginSlice.ts
+++ frontend/src/features/loginSlice.ts
@@ -1,23 +1,28 @@
 import { createSlice, PayloadAction } from '@reduxjs/toolkit'
 
 type LoginStat = {
-  loggedIn: boolean
+    loggedIn: boolean
+    isAdmin: boolean
 }
 
 const initialState: LoginStat = {
-  loggedIn: false,
+    loggedIn: false,
+    isAdmin: false,
 }
 
 export const loginSlice = createSlice({
-  name: 'loggin',
-  initialState,
-  reducers: {
-    setLoggedIn: (state, action: PayloadAction<boolean>) => {
-      state.loggedIn = action.payload
+    name: 'loggin',
+    initialState,
+    reducers: {
+        setLoggedIn: (state, action: PayloadAction<boolean>) => {
+            state.loggedIn = action.payload
+        },
+        setIsAdmin: (state, action: PayloadAction<boolean>) => {
+            state.isAdmin = action.payload
+        },
     },
-  },
 })
 
-export const { setLoggedIn } = loginSlice.actions
+export const { setLoggedIn, setIsAdmin } = loginSlice.actions
 
 export default loginSlice.reducer
