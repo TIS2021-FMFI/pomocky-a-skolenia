a71928c315248d91adc6064997f3ae4bb4af1c00
Jackulik3 <91477400+Jackulik3@users.noreply.github.com>
2022-02-08 21:42:25 +0100

    Update admin.js

--- backend/admin.js
+++ backend/admin.js
@@ -1,3 +1,14 @@
-console.log("admin script")
+const { pool } = require('./database/database.js')
+const {hashPassword} = require('./functions.js')
+
 const heslo = process.argv[2]
-console.log(heslo)
+
+async function run(){
+    const encryptedPassword = await hashPassword(heslo)
+
+    const queryText2 = 'INSERT INTO pouzivatelia(email, heslo, is_admin) VALUES ($1, $2, true) ON conflict(email) DO update set heslo=EXCLUDED.heslo'
+
+    await pool.query(queryText2, ["admin", encryptedPassword])
+}
+
+run().then()
